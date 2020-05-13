ab8f746aa8919dafd49aa456d81c813b67abe3d9
Brit <me@brittany.codes>
2020-05-13 14:15:33 -0400

    Initial commit

---
+++ example/todos_repository_example.dart
@@ -0,0 +1,6 @@
+import 'package:todos_repository/todos_repository.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/todos_repository_test.dart
@@ -0,0 +1,16 @@
+import 'package:todos_repository/todos_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
